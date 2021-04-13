923e9342362be2439e5072940c402722624d03d7
Kalbiq <hubert.kalbarczyk@gmail.com>
2021-04-13 16:46:16 +0200

    dice juz troche bardziej sens ma

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -1,11 +1,24 @@
+// zasady jbc to Yahtzee
+// np. https://www.dicegamedepot.com/yahtzee-rules/
+
 package Dice;
 
+
 public class Dice {
-    int score;
     int[] dice = {0, 0, 0, 0, 0, 0};
+    int reroll;
+
+    // Yahtzee goals as on a standard score sheet (13)
+    int[] goals;
+    int score;
 
     public Dice() {
         this.score = 0;
+        this.reroll = 2;
+        this.goals = new int[13];
+        for(int i = 0; i < 13; i++){
+            this.goals[i] = -1;
+        }
     }
 
     public void rollDice() {
@@ -15,9 +28,13 @@ public class Dice {
             roll = (int)Math.floor(Math.random() * (7 - 1)) + 1;
             this.dice[i] = roll;
         }
+        this.reroll = 2;
     }
 
-    public boolean checkIfWin() {
-        return false;
+    public void rollOneDice(int dice_num) {
+        if(dice_num > 0 && dice_num < 7 && this.reroll > 0){
+            this.dice[dice_num] = (int)Math.floor(Math.random() * (7 - 1)) + 1;
+            this.reroll--;
+        }
     }
 }
--- src/Dice/TestDice.java
+++ src/Dice/TestDice.java
@@ -4,13 +4,6 @@ import Dice.Dice;
 
 public class TestDice {
     public static void main(String[] args){
-        Dice dice_game = new Dice();
-        int winc = 0;
-        for(int i = 0; i < 10000; i++){
-            dice_game.rollDice();
-            if(dice_game.checkIfWin())
-                winc++;
-        }
-        System.out.printf("%d\n", winc);
+
     }
 }
