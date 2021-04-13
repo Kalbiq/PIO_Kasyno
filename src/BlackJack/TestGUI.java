c3150084b5e6ada0ca4890b9e854c9bea33ea036
stanlej909 <82446265+stanlej909@users.noreply.github.com>
2021-04-13 18:51:30 +0200

    Es

--- src/BlackJack/TestGUI.java
+++ src/BlackJack/TestGUI.java
@@ -8,9 +8,7 @@ public class TestGUI {
         EventQueue.invokeLater(()->
         {
            var frame=new SimpleFrame(WIDTH, HEIGHT);
-           JPanel panel = new JPanel();
-           panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-
+           frame.setTitle("Es");
            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
            frame.setVisible(true);
         });
