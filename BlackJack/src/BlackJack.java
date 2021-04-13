be106002bb35d5d778a8bf563719c6bba1426719
Paweł <tpawel890098@o2.pl>
2021-04-13 16:14:53 +0200

    Dodanie klasy graczy

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -1,14 +1,28 @@
 
 public class BlackJack {
 
-
-
-
     public static void main(String[] args)
     {
         Deck e= new Deck();
         e.shuffleDeck();
         System.out.println(e.decktoshuffle);
+
+        Player player=new Player();
+        Player enemy=new Player();
+
+
+
+        for(int i=0;i<9;i++)
+        {
+            player.addCard(e.drawCard());
+            enemy.addCard(e.drawCard());
+        }
+
+        System.out.println("Reka gracza:");
+        System.out.println(player.player_hand);
+        System.out.println("Reka krupiera:");
+        System.out.println(enemy.player_hand);
+
     }
 
 }
--- BlackJack/src/Deck.java
+++ BlackJack/src/Deck.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class Deck {
+
+    public int deck_it=0;
     public static String[] deck = new String[] {"H2","S2","D2","C2","H3","S3","D3","C3",
             "H4","S4","D4","C4","H5","S5","D5","C5","H6","S6","D6","C6","H7","S7","D7","C7","H8","S8","D8","C8",
             "H9","S9","D9","C9","HT","ST","DT","CT","HJ","SJ","DJ","CJ","HQ","SQ","DQ","CQ","HK","SK","DK","HA",
@@ -15,6 +17,12 @@ public class Deck {
 
     public void shuffleDeck(){
         Collections.shuffle(decktoshuffle);
+    }
+
+    public String drawCard()
+    {
+        deck_it++;
+                return decktoshuffle.get(deck_it-1);
     }
 
 }
