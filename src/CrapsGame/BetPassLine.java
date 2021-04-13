c9e17e2cab0ee12e38e76cf62111eaea176ebadb
Kalbiq <hubert.kalbarczyk@gmail.com>
2021-04-13 21:10:17 +0200

    PassLine and DontPassLine

--- src/CrapsGame/BetPassLine.java
+++ src/CrapsGame/BetPassLine.java
@@ -2,15 +2,14 @@ package CrapsGame;
 
 import java.util.Arrays;
 
-public class PassLine extends Bet {
+public class BetPassLine extends Bet {
     int multiplier = 1;
     String name = "Pass Line";
-    String description = "Bet on 7 and 11";
+    String description = "Bet on 7 or 11";
 
     @Override
     public boolean check(int[] dice) {
         int sumDice = Arrays.stream(dice).sum();
-
         return sumDice == 7 || sumDice == 11;
     }
 }
