0a6b225c7b0e14da256155de6bf10a8b34f25e2c
Kacper Baczynski <01112162@pw.edu.pl>
2021-05-11 17:30:03 +0200

    Przeniesienie obrazow kostek do osobnego folderu

--- src/Craps/DiceImg.java
+++ src/Craps/DiceImg.java
@@ -14,7 +14,7 @@ public class DiceImg extends JLabel {
 
     public void setImg(String name){
         this.imgName = name;
-        this.img = new ImageIcon("images/" + imgName + ".png");
+        this.img = new ImageIcon("images/dices/" + imgName + ".png");
         this.setIcon(img);
         this.setVerticalAlignment(JLabel.CENTER);
     }
